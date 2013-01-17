c7861ee3843e2311faebb77e22314bfcf7ec5eaf
Josh McMillan <josh@gamaroff.co.uk>
2013-01-17 11:27:35 +0000

    Cleanup; adhering to Fuel's coding standards a bit better

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
 Autoloader::add_classes(array(
+
 	'Agent' => __DIR__.'/classes/agent.php',
+
 ));
--- classes/agent.php
+++ classes/agent.php
@@ -1,18 +1,21 @@
 <?php
 
-class Agent extends \Fuel\Core\Agent {
+class Agent extends \Fuel\Core\Agent
+{
 	
-	public static function is_mobiledevice() {
+	public static function is_mobiledevice()
+	{
 
 		return static::mobile_detect_instance()->isMobile();
 
 	}
 
-	protected static function mobile_detect_instance() {
+	protected static function mobile_detect_instance()
+	{
 
-		require_once PKGPATH . 'special_agent' . DS . 'vendor' . DS . 'php-mobile-detect' . DS . 'Mobile_Detect.php';
+		require_once PKGPATH.'special_agent'.DS .'vendor'.DS.'php-mobile-detect'.DS.'Mobile_Detect.php';
 
-		$instance = new Mobile_Detect;
+		$instance = new Mobile_Detect();
 
 		return $instance;
 
